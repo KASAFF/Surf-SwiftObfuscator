7c536f0faaa49371ff37b232d2f5b2df74103ad9
Alexey Kosov <kasaff322@icloud.com>
2024-02-26 19:27:51 +0400

    refactor: refactor localizationStrings

--- Sources/SwiftObfuscator/Commands/ObfuscateStrings.swift
+++ Sources/SwiftObfuscator/Commands/ObfuscateStrings.swift
@@ -15,14 +15,14 @@ struct ObfuscateStrings: ParsableCommand {
     @Option(name: .shortAndLong, help: "The line number on which strings should be obfuscated. By default, all lines will be obfuscated.")
     var line: Int?
 
-    @Option(name: .shortAndLong, help: "The usage for localizable")
-    var aboba: Bool?
+    @Option(name: .long, help: "This property used for obfuscate SwiftGen localizable strings")
+    var swiftGen: Bool?
 
     mutating func run() throws {
         let contents = try String(contentsOfFile: filePath, encoding: .utf8)
         let obfuscator = StringObfuscator(contents: contents, salt: salt)
         let newContents: String
-        if let aboba {
+        if let swiftGen {
             newContents = obfuscator.obfuscateLocalizationValues()
         } else {
             newContents = try obfuscator.obfuscate(line: line)
